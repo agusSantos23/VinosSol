daf26557fd87c74a2db44c55be0960a3ec049fa7
AguSSantos Prieto Atienza <131393036+agusSantos23@users.noreply.github.com>
2024-10-29 20:14:16 +0100

    Wine 0.3

--- src/pages/LadingPage.jsx
+++ src/pages/LadingPage.jsx
@@ -5,9 +5,11 @@ const LadingPage = () => {
 
   return (
     <div>
-      <Wine color="#6b033b" text="Generoso"/>
+      <Wine color="#6b033b" text="Tinto" invested={true}/>
+      <Wine color="#cc711d" text="Generoso" invested={false}/>
 
-      <Wine color="#cc711d" front={false}/>
+
+      <Wine color="#4f073e"/>
     </div>
   )
 }
