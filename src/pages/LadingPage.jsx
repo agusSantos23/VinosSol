da0be1c2f9f14e18aad854aedfc36d4d6be4edbc
AguSSantos Prieto Atienza <131393036+agusSantos23@users.noreply.github.com>
2024-10-29 20:18:09 +0100

    Wine 0.4

--- src/pages/LadingPage.jsx
+++ src/pages/LadingPage.jsx
@@ -1,15 +1,10 @@
 
-import Wine from "../components/common/Wine.jsx"
 
 const LadingPage = () => {
 
   return (
     <div>
-      <Wine color="#6b033b" text="Tinto" invested={true}/>
-      <Wine color="#cc711d" text="Generoso" invested={false}/>
-
-
-      <Wine color="#4f073e"/>
+      
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,4 @@
-/** @type {import("tailwindcss").Config} */
+/** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./index.html",
@@ -7,26 +7,8 @@ export default {
   theme: {
     extend: { 
       fontFamily: { 
-        lora: ["Lora", "sans-serif"], 
+        lora: ['Lora', 'sans-serif'], 
       }, 
-      animation: {
-        swinging: "swinging 0.7s ease-in-out",
-        swingingShadow: "swingingShadow 0.7s ease-in-out",
-
-      },
-      keyframes: {
-        swinging: {
-          "25%": { transform: "rotate(-5deg)" },
-          "50%": { transform: "rotate(5deg)" },
-          "75%": { transform: "rotate(-5deg)" },
-        },
-        swingingShadow: {
-          "25%": { transform: "translateX(-2px)" },
-          "50%": { transform: "translateX(2px)" },
-          "75%": { transform: "translateX(-2px)" },
-        },
-      },
-      
     },
   },
   plugins: [],
