aeaba642bbd80b5e77994ff9aec9591c71d40448
AguSSantos Prieto Atienza <131393036+agusSantos23@users.noreply.github.com>
2024-10-31 14:55:55 +0100

    Wine 0.5

--- src/pages/LadingPage.jsx
+++ src/pages/LadingPage.jsx
@@ -4,7 +4,7 @@ const LadingPage = () => {
 
   return (
     <div>
-      
+
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,8 +7,25 @@ export default {
   theme: {
     extend: { 
       fontFamily: { 
-        lora: ['Lora', 'sans-serif'], 
+        lora: ["Lora", "sans-serif"], 
       }, 
+      animation: {
+        swinging: "swinging 0.7s ease-in-out",
+        swingingShadow: "swingingShadow 0.7s ease-in-out",
+      },
+      keyframes: {
+        swinging: {
+          "25%": { transform: "rotate(-5deg)" },
+          "50%": { transform: "rotate(5deg)" },
+          "75%": { transform: "rotate(-5deg)" },
+        },
+        swingingShadow: {
+          "25%": { transform: "translateX(-2px)" },
+          "50%": { transform: "translateX(2px)" },
+          "75%": { transform: "translateX(-2px)" },
+        },
+      },
+      
     },
   },
   plugins: [],
