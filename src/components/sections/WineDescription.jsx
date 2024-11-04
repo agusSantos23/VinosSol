901dcc37e46d6e926d724d143ad49f95a134a83e
pakito072 <105859870+pakito072@users.noreply.github.com>
2024-11-04 13:37:49 +0100

    Wine Description 0.2

--- src/components/sections/WineDescription.jsx
+++ src/components/sections/WineDescription.jsx
@@ -2,14 +2,57 @@ import PropTypes from 'prop-types'
 
 const WineDescription = ({wine}) => {
 
+    switch (wine) {
+      case 1:
+        
+
+        break;
+
+      case 2:
+
+
+        break;
+
+      case 3:
+
+
+        break;
+
+      case 4:
+
+
+        break;
+
+      case 5:
+
+
+        break;
+
+        case 6:
+
+
+          break;
     
+      default:
+        break;
+    }
 
 
   return (
 
-    <section className="relative h-screen w-screen bg-secondary">
-        <img src="../src/assets/img/redWine.png" alt="redWine" className="absolute top- left-3 h-2/4 w-auto rounded-md" />
+    <section className="flex justify-center items-center h-screen w-screen p-14 bg-secondary">
+      
+      <img src="../src/assets/img/redWine.png" alt="redWine" className=" w-1/3 h-auto rounded-md" />
 
+      <div className="w-4/6 h-5/6 bg-amber-300  text-white">
+        
+        <h1 className="text-center text-4xl font-bold">Red wine</h1>
+        <div className='pt-4 p'>
+        <p className='w-1/2'>Red wine is a type of wine that is made from the skin of the red grape. It is typically made from a variety of grapes, including Cabernet Sauvignon, Merlot, Pinot Noir, and Syrah.</p>
+        <p className='w-1/2 start'> Red wine is typically made from a variety of grapes, including Cabernet Sauvignon, Merlot, Pinot Noir, and Syrah.</p>
+        </div>
+      </div>
+    
     </section>
     
 
